bebd4d27f70a4f0285886009e0ea8204f3208355
Sid Engel <me@sidengel.com>
2023-05-11 15:28:56 -0600

    File name contains domain

--- Reporting - CSV Of Prem Domain Admins.ps1
+++ Reporting - CSV Of Prem Domain Admins.ps1
@@ -1,3 +1,4 @@
-# Simple 1 liner.
-# Dumps a CSV onto C:\.
-get-adgroupmember 'domain admins' | select samaccountname | Export-CSV C:\domain-admins-list.csv
+# Dumps a CSV to C:\Admin-List DOMAIN.COM.csv
+$domain = (Get-ADDomain).DNSRoot
+$filename= 'c:\Admin-List ' + $domain + '.csv'
+$list = get-adgroupmember 'domain admins' | select samaccountname | Export-CSV $filename
