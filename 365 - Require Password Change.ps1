8fc7de0b60aef6e5d9bd437926fc62d6fa52febb
Sid Engel <me@sidengel.com>
2023-07-21 12:58:48 -0600

    Create 365 - Require Password Change.ps1

---
+++ 365 - Require Password Change.ps1
@@ -0,0 +1,3 @@
+Set-MsolUserPassword -UserPrincipalName user@domain.com -ForceChangePassword:$true -ForceChangePasswordOnly:$true
+
+# NEED TO ADD TO THIS, SO IT PULLS THE UPN LIST AND CYCLES THROUGH THEM WITH THE COMMAND.
