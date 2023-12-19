8ec7dd24ea243bb11da326135a5fb90a104872fc
Sid Engel <me@sidengel.com>
2023-12-19 11:55:02 -0700

    Create 365-enforce-mfa-on-enabled-users.ps1

---
+++ 365-enforce-mfa-on-enabled-users.ps1
@@ -0,0 +1,30 @@
+# Connect to tenant
+Connect-MsolService
+
+# Sets the MFA requirement state
+function Set-MfaState {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipelineByPropertyName=$True)]
+        $ObjectId,
+        [Parameter(ValueFromPipelineByPropertyName=$True)]
+        $UserPrincipalName,
+        [ValidateSet("Disabled","Enabled","Enforced")]
+        $State
+    )
+    Process {
+        Write-Verbose ("Setting MFA state for user '{0}' to '{1}'." -f $ObjectId, $State)
+        $Requirements = @()
+        if ($State -ne "Disabled") {
+            $Requirement =
+                [Microsoft.Online.Administration.StrongAuthenticationRequirement]::new()
+            $Requirement.RelyingParty = "*"
+            $Requirement.State = $State
+            $Requirements += $Requirement
+        }
+        Set-MsolUser -ObjectId $ObjectId -UserPrincipalName $UserPrincipalName `
+                     -StrongAuthenticationRequirements $Requirements
+    }
+}
+# Disable MFA for all users
+Get-MsolUser -All | Set-MfaState -State Disabled
